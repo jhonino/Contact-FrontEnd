b78ad14cb7492d3d5a90e835b27012300a56cd54
jhonino <jinofuenteap@gmail.com>
2025-05-24 11:58:59 -0500

    agregando metodos de comunicacion al servicio

--- src/app/servicios/contact.service.ts
+++ src/app/servicios/contact.service.ts
@@ -8,5 +8,25 @@ export class ContactService {
 
   private http = inject(HttpClient);   //nueva forma de hacer inyeccion, no utiliza contructor
 
-  constructor() { }
+  //metodos
+  list(){
+    return this.http.get('http://localhost:8080/api/contact');
+  }
+
+  listXid(id : number){
+    return this.http.get(`http://localhost:8080/api/contact/${id}`);
+  }
+
+  create(contact: any){
+    return this.http.post('http://localhost:8080/api/contact', contact);
+  }
+
+  update(contact: any, id : number){
+    return this.http.put(`http://localhost:8080/api/contact/${id}`, contact);
+  }
+
+  delete(id:number){
+    return this.http.delete(`http://localhost:8080/api/contact/${id});
+  }
+
 }
