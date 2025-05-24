b4cb49f428f4da578ebe875dd75040b9b26b5585
jhonino <jinofuenteap@gmail.com>
2025-05-24 18:11:05 -0500

    agregando datos o almancenado datos del formulario para mandar al api

--- src/app/componentes/contact-form/contact-form.component.ts
+++ src/app/componentes/contact-form/contact-form.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-contact-form',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],  //se importa el reactiveformsmodule para formularios reactivos
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  private fb = inject(FormBuilder);
 
+  form = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]]
+  })
 }
